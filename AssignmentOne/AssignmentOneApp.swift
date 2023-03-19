e40fccd4e9c15ea3b20ddb061d5dfd8b5e2bbd22
Steele Cooke <steelecooke+github@gmail.com>
2023-03-19 20:54:24 +1000

    Initial Commit

---
+++ AssignmentOne/AssignmentOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOneApp.swift
+//  AssignmentOne
+//
+//  Created by Steele Cooke on 19/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
