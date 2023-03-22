536639bd6a2fba9f45ec0d62297e2d1b879ab704
Steele Cooke <steelecooke+github@gmail.com>
2023-03-22 20:58:22 +1000

    Update - Expanded the unit test for the generate list function into additional functions and added more tests to validate the example data.

--- AssignmentOneTests/AssignmentOneTests.swift
+++ AssignmentOneTests/AssignmentOneTests.swift
@@ -18,19 +18,39 @@ final class AssignmentOneTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    // Check that the generated example list is returning a valid list
-    func testExampleList() throws {
+    // Check if the generated example list contains the expected number of items
+    func testExampleListCount() throws {
         let list = generateExampleList()
         
-        // Check that the example list doesn't contain any nil data
+        XCTAssertEqual(list.count, 10)
+    }
+    
+    // Check if the checked state of the items in the example list is as expected
+    func testExampleListCheckedStates() throws {
+        let list = generateExampleList()
+        let expectedCheckedStates = [true, false, true, false, true, false, true, false, true, false]
+        
+        for (index, item) in list.enumerated() {
+            XCTAssertEqual(item.checked, expectedCheckedStates[index])
+        }
+    }
+    
+    // Check if the generated example list is returning a valid list with no nil data
+    func testExampleListItems() throws {
+        let list = generateExampleList()
+        
         for item in list {
             XCTAssertNotNil(item)
             XCTAssertNotNil(item.id)
             XCTAssertNotNil(item.description)
             XCTAssertNotNil(item.checked)
         }
+    }
+
+    // Check if the first and last items in the list have their description value as expected
+    func testExampleListDescriptions() throws {
+        let list = generateExampleList()
         
-        // Confirm that the first and last items in the example list have the correct description
         XCTAssertEqual(list[0].description, "Pay electricity bill")
         XCTAssertEqual(list[9].description, "Pay credit card bill by the end of the month")
     }
