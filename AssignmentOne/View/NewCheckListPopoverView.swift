3b19cfb0c9ae7936e75154b242313172d41cef44
Steele Cooke <steelecooke+github@gmail.com>
2023-04-05 19:56:42 +1000

    Update - Added comments.

--- AssignmentOne/View/NewCheckListPopoverView.swift
+++ AssignmentOne/View/NewCheckListPopoverView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+/// A view that displays a popover for creating a new checklist.
+/// - Parameters:
+///     - checkLists: [CheckList] - An array of `CheckList` objects representing all CheckLists in the app.
+/// `NewCheckListPopoverView`uses it's associated viewModel NewCheckListPopoverViewModel to append to the checklist as required..
+/// - Examples:
+///     ```swift
+/// ForEach(viewModel.checkLists.indices, id: \.self) { index in
+///     let list = viewModel.checkLists[index]
+///     if !list.completed {
+///         NavigationLink(destination: CheckListView(list: $viewModel.checkLists[index].items, name: $viewModel.checkLists[index].name)) {
+///             ListCheckListRow(checkList: $viewModel.checkLists[index])
+///         }
+///     }
+/// }
+///     ```
 struct PopoverContentView: View {
     @Environment(\.dismiss) var dismiss
 
--- AssignmentOne/ViewModel/NewCheckListPopoverViewModel.swift
+++ AssignmentOne/ViewModel/NewCheckListPopoverViewModel.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
+/// The ViewModel for the associated view NewCheckListPopoverView.
+///
+/// `NewCheckListPopoverViewModel` is responsible for handling business logic for the it's associated view.
+/// It handles updating a provided list of CheckList.
+/// - Functions:
+///     - addEmptyCheckList - Creates an empty CheckList and appends it to a passed [CheckList] variable returning the updated [CheckList].
 class NewCheckListPopoverViewModel: ObservableObject {
 
+    /// Creates an empty CheckList and appends it to a passed [CheckList] variable returning the updated [CheckList].
+    /// - Parameters:
+    ///   - name - String - A name to be given to the new empty CheckList
+    ///   - checkLists - [CheckList] - An existing [CheckList] variable to appened the empty list to and return.
+    /// - Returns: [CheckList]
+    /// - Examples:
+    ///     ```swift
+    /// checkLists = viewModel.addEmptyCheckList(name: name, checkLists: checkLists)
+    ///     ```
     func addEmptyCheckList(name: String, checkLists: [CheckList]) -> [CheckList] {
         let items: [ListItem] = []
         var lists = checkLists
